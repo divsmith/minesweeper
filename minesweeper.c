5bdfa4b6fae9a8b79ab0074e38187d171da25486
Parker Smith <parker@parkersmith.us>
2016-12-09 12:08:56 -0700

    Remove test code

--- minesweeper.c
+++ minesweeper.c
@@ -44,22 +44,7 @@ int main() {
 
 		PrintGrid();
 	}
-//	grid[0][0].adjacentMines = 4;
-//	grid[1][2].isMine = false;
-//	grid[1][3].isMine = false;
-//
-//	printf("%d\n", grid[0][0].adjacentMines);
-//	printf("%d\n", grid[1][2].isMine);
-//	printf("%d\n", grid[1][3].isMine);
-//
-//	if (!grid[1][2].isMine)
-//	{
-//		printf("%s", "not a mine");
-//	}
-//
-//	if (grid[1][2].isMine) {
-//		printf("%s", "is a mine");
-//	}
+	
 	exit(0);
 }
 
